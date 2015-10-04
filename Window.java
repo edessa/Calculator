10fa0177961c3e4945cc939809d554e5862fb21e
edessa <edessale@gmu.edu>
2015-10-03 19:39:31 -0700

    Update Window.java

--- Window.java
+++ Window.java
@@ -640,56 +640,14 @@ public class Window extends JFrame implements ActionListener, WindowListener{
 		else{
 			
 			//Setup the EULA text
-			JTextArea text = new JTextArea(
-					"The product is protected by Federal Copyright Law. PNDesignSolutions, LLC " +
-					"retains the title to and ownership of the Product. You are licensed to " +
-					"use this Product on the following terms and conditions:\nLICENSE - The " +
-					"licensee is defined as the individual or company utilizing the Software " +
-					"Product. PNDesignSolutions, LLC hereby grants the licensee a nonexclusive " +
-					"license authorizing the licensee to use the enclosed Product on one " +
-					"computer at a time. The licensee is also permitted to distribute this " +
-					"product to one, and only one web server to host the Program. Please " +
-					"contact PNDesignSolutions, LLC if you require additional licenses. Use " +
-					"of this product by more than one individual or by anyone other than the " +
-					"licensee terminates, without notification, this license and the right to " +
-					"use this product.\nYOU MAY NOT - Distribute, rent, sub-license or " +
-					"otherwise make available to others the software or documentation or " +
-					"copies thereof, except as expressly permitted in this License without " +
-					"prior written consent from PNDesignSolutions, LLC In the case of an " +
-					"authorized transfer, the transferee must agree to be bound by the terms " +
-					"and conditions of this License Agreement.\nRESTRICTIONS - You may use " +
-					"this Product in your business as long as: The software serial number and " +
-					"user must be registered with PNDesignSolutions, LLC in order to receive " +
-					"support or distribution rights. You may not remove any proprietary " +
-					"notices, labels, trademarks on the software or documentation. You may not " +
-					"modify, de-compile, disassemble, reverse engineer or translate the " +
-					"software.\nUS GOVERNMENT RESTRICTED RIGHTS - Use, duplication or " +
-					"disclosure by the United States Government is subject to restrictions as " +
-					"set forth under DFARS 252.227-7013 or in FARS 52.227-19 Commercial " +
-					"Computer Software - Restricted Rights.\nTERM - You may terminate your " +
-					"License and this Agreement at anytime by destroying all copies of the " +
-					"Product and Product Documentation. They will also terminate " +
-					"automatically if you fail to comply with any term or condition in this " +
-					"Agreement.\nLIMITED WARRANTY - This software and documentation are sold " +
-					"'as is' without any warranty as to their performance, merchantability or " +
-					"fitness for any particular purpose. The licensee assumes the entire risk " +
-					"as to the quality and performance of the software. PNDesignSolutions, LLC " +
-					"warrants that the diskettes on which the Program is furnished will be " +
-					"free from any defects in materials. Exclusive remedy in the event of a " +
-					"defect is expressly limited to the replacement of diskettes. In no event " +
-					"shall PNDesignSolutions, LLC or anyone else who has been involved in the " +
-					"creation, development, production, or delivery of this software be liable " +
-					"for any direct, incidental or consequential damages, such as, but not " +
-					"limited to, loss of anticipated profits, benefits, use, or data " +
-					"resulting from the use of this software, or arising out of any breach of " +
-					"warranty.", 15, 25);
+			JTextArea text = new JTextArea(15, 25);
 			text.setEditable(false);
 			text.setLineWrap(true);
 			text.setWrapStyleWord(true);
 			
 			//Open dialog
 			String[] o = {"Accept", "Deny"};
-			int i = JOptionPane.showOptionDialog(this, new JScrollPane(text), "End-User License Agreement", 
+			int i = JOptionPane.showOptionDialog(this, new JScrollPane(text), 
 					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, o, o[1]);
 			
 			//If the user clicks ACCEPT
